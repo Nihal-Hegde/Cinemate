700b5aa4ae90dc0bb41a2ddcf4ddcbab1b8f6516
Nihal-Hegde <nihalhegde6@gmail.com>
2025-07-27 19:29:14 +0530

    UpdatedNewonw

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ export const Footer = () => {
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
      
-      //[eslint]
+      [eslint]
         <li>
             <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
         </li>
