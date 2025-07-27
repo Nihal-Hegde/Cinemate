4b3ec86ae34ffb55587d9cf8dddd155d1db0dab4
Nihal-Hegde <nihalhegde6@gmail.com>
2025-07-27 19:27:12 +0530

    NewwithFooter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,8 @@ export const Footer = () => {
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">Cinemate™</Link>. All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-      //eslint-disable-next-line
+     
+      //[eslint]
         <li>
             <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
         </li>
