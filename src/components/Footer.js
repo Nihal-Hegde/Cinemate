c8cd6f1623ecbfe28c558994f4d89870bf9fde0e
Nihal-Hegde <nihalhegde6@gmail.com>
2025-07-28 12:55:07 +0530

    Changes to dev

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,19 @@ export const Footer = () => {
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
      
-      
+      [eslint]
+        <li>
+            <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
+        </li>
+        <li>
+            <a href="#" target="_blank"  className="hover:underline me-4 md:me-6">LinkedIn</a>
+        </li>
+        <li>
+            <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Youtube</a>
+        </li>
+        <li>
+            <a href="#" target="_blank" className="hover:underline">Github</a>
+        </li>
     </ul>
     </div>
 </footer>
